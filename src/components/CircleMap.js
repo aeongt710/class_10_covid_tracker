f9c40855561889f6c5911c6b7f37d054fb9b594f
unknown <unknowndonlion@gmail.com>
2022-02-04 00:21:46 +0500

    num format added

--- src/components/CircleMap.js
+++ src/components/CircleMap.js
@@ -12,11 +12,26 @@ import {
 } from "react-map-gl";
 import uuid from "react-uuid";
 import { useRef, useEffect, useState } from "react";
-import { makeStyles } from "@mui/material";
+import { Card, makeStyles } from "@mui/material";
 // import mapboxgl from '!mapbox-gl';
 // import mapboxgl from '!mapbox-gl';
 function CircleMap({ center, zoom, list }) {
-  //   console.log("inside map", center);
+  const [viewport, setViewport] = React.useState({
+    longitude: center.lng,
+    latitude: center.lat,
+    bearing: 0,
+    pitch: 0,
+    zoom: zoom,
+  });
+  useEffect(() => {
+    console.log("rerendered");setViewport({
+      longitude: center.lng,
+      latitude: center.lat,
+      bearing: 0,
+      pitch: 0,
+      zoom: zoom,
+    });
+  }, [center]);
   //dummy
   //   const [viewport, setViewport] = useState({
   //     width: 800,
@@ -25,105 +40,108 @@ function CircleMap({ center, zoom, list }) {
   //     longitude: 70,
   //     zoom: 4,
   //   });
-  const geolocateStyle = {
-    top: 0,
-    left: 0,
-    padding: '10px'
-  };
-  
-  const fullscreenControlStyle = {
-    top: 36,
-    left: 0,
-    padding: '10px'
-  };
-  
-  const navStyle = {
-    top: 72,
-    left: 0,
-    padding: '10px'
-  };
-  
-  const scaleControlStyle = {
-    bottom: 36,
-    left: 0,
-    padding: '10px'
-  };
-  const [viewport, setViewport] = React.useState({
-    longitude: 70,
-    latitude: 30,
-    bearing: 0,
-    pitch: 0,
-    zoom: zoom,
-  });
+  //   const geolocateStyle = {
+  //     top: 0,
+  //     left: 0,
+  //     padding: "10px",
+  //   };
+
+  //   const fullscreenControlStyle = {
+  //     top: 36,
+  //     left: 0,
+  //     padding: "10px",
+  //   };
+
+  //   const navStyle = {
+  //     top: 72,
+  //     left: 0,
+  //     padding: "10px",
+  //   };
+
+  //   const scaleControlStyle = {
+  //     bottom: 36,
+  //     left: 0,
+  //     padding: "10px",
+  //   };
+
   const [selectedCountry, setSelectedCountry] = useState(null);
   //   console.log(list);
 
   const [showPopup, togglePopup] = React.useState(false);
   return (
-    <div className="map">
-      <ReactMapGL
-        {...viewport}
-        onViewportChange={(nextViewport) => setViewport(nextViewport)}
-        onClick={(e) => {
-          togglePopup(false);
-        }}
-        mapboxApiAccessToken="pk.eyJ1Ijoiam9udGF5eXciLCJhIjoiY2s4aXcwbnA0MGFqYjNscDZicm9haXA3cCJ9.rI3D6Y4ZETQnYukX9RCOow"
-        width={"100%"}
-        height={"100%"}
-        // mapStyle="mapbox://styles/mapbox/dark-v10"
-      >
-        {list.map((country) => (
-          <Marker
-            longitude={country.countryInfo.long}
-            latitude={country.countryInfo.lat}
-          >
-            <button
-              style={{
-                border: "none",
-                backgroundColor: "transparent",
-                outline: "none",
-              }}
-              onClick={(e) => {
-                e.preventDefault();
-                setSelectedCountry(country);
+    <Card className="asd">
+      <div className="map">
+        <ReactMapGL
+          {...viewport}
+          onViewportChange={(nextViewport) => setViewport(nextViewport)}
+          onClick={(e) => {
+            togglePopup(false);
+          }}
+          mapboxApiAccessToken="pk.eyJ1Ijoiam9udGF5eXciLCJhIjoiY2s4aXcwbnA0MGFqYjNscDZicm9haXA3cCJ9.rI3D6Y4ZETQnYukX9RCOow"
+          width={"100%"}
+          height={"100%"}
+          // mapStyle="mapbox://styles/mapbox/dark-v10"
+        >
+          {list.map((country) => (
+            <Marker
+              longitude={country.countryInfo.long}
+              latitude={country.countryInfo.lat}
+            >
+              <button
+                style={{
+                  border: "none",
+                  backgroundColor: "transparent",
+                  outline: "none",
+                }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSelectedCountry(country);
 
-                togglePopup(!showPopup);
-              }}
+                  togglePopup(!showPopup);
+                }}
+              >
+                <div>
+                  <img src={country.countryInfo.flag} width="40" height="25" />
+                </div>
+              </button>
+            </Marker>
+          ))}
+          {showPopup && (
+            <Popup
+              longitude={selectedCountry.countryInfo.long}
+              latitude={selectedCountry.countryInfo.lat}
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => togglePopup(false)}
+              anchor="top-right"
             >
               <div>
-                <img src={country.countryInfo.flag} width="40" height="25" />
+                {console.log(selectedCountry)}
+                <strong>{selectedCountry.countryInfo.iso3}</strong>
+                <br />
+                Cases: <strong>{selectedCountry.cases}</strong>
+                <br />
+                Deaths: <strong>{selectedCountry.deaths}</strong>
+                <br />
+                Recovered: <strong>{selectedCountry.recovered}</strong>
+                <br />
+                Continent: <strong>{selectedCountry.continent}</strong>
+                <br />
+                Location:{" "}
+                <strong>
+                  [{selectedCountry.countryInfo.lat},
+                  {selectedCountry.countryInfo.long}]
+                </strong>
               </div>
-            </button>
-          </Marker>
-        ))}
-        {showPopup && (
-          <Popup
-            longitude={selectedCountry.countryInfo.long}
-            latitude={selectedCountry.countryInfo.lat}
-            closeButton={true}
-            closeOnClick={false}
-            onClose={() => togglePopup(false)}
-            anchor="top-right"
-          >
-            <div>
-              {console.log(selectedCountry)}
-              <strong>{selectedCountry.countryInfo.iso3}</strong>
-              <br />
-              Cases: <strong>{selectedCountry.cases}</strong>
-              <br />
-              Deaths: <strong>{selectedCountry.deaths}</strong><br />
-              Recovered: <strong>{selectedCountry.recovered}</strong><br />
-              Continent: <strong>{selectedCountry.continent}</strong><br />
-              Location: <strong>[{selectedCountry.countryInfo.lat},{selectedCountry.countryInfo.long}]</strong>
-            </div>
-          </Popup>
-        )}
-        {/* <GeolocateControl style={geolocateStyle} />
+            </Popup>
+          )}
+          {/* <GeolocateControl style={geolocateStyle} />
         <FullscreenControl />
         <NavigationControl className="abc" />
         <ScaleControl  /> */}
-      </ReactMapGL>
-    </div>
+        </ReactMapGL>
+      </div>
+    </Card>
   );
 }
 
--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,6 +1,6 @@
 import { Card, CardContent, Typography } from '@mui/material';
 import React from 'react';
-
+import NumberFormat from 'react-number-format';
 function InfoBox({title,cases,total}) {
     return (
         <Card className="infoBox">
@@ -8,8 +8,9 @@ function InfoBox({title,cases,total}) {
                 <Typography color="textSecondary"  className="infoBox_title">
                     {title}
                 </Typography>
-                <h2  className="infoBox_cases">{cases}</h2>
-                <Typography color="textSecondary"  className="infoBox_total">{total}</Typography>
+                
+                <h2  className="infoBox_cases"><NumberFormat value={cases} displayType={'text'} thousandSeparator={true} prefix={''} /></h2>
+                <Typography color="textSecondary"  className="infoBox_total"><NumberFormat value={total} displayType={'text'} thousandSeparator={true} prefix={''} /></Typography>
             </CardContent>
         </Card>
     );
